cce96c98992cbb0b2a8590ee24332f112e091940
realrgt <ergitorubildo@gmail.com>
2020-02-21 08:32:04 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+    name: { type: String, required: [true, 'Name is required'] },
+    email: { type: String, unique: true, required: [true, 'Email is required'] },
+    password: { type: String, required: [true, 'Password is required'] },
+    img: { type: String },
+    Role: { type: String, required: true, default: 'USER_ROLE' },
+
+});
+
+module.exports = mongoose.model('User', userSchema);
