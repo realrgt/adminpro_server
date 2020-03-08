8e4153c8766fd84a352fd6e78dd576512f480173
realrgt <ergitorubildo@gmail.com>
2020-03-08 09:44:33 +0200

    created token

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -11,25 +12,31 @@ router.post("/", (req, res, next) => {
     .exec()
     .then(user => {
       if (!user) {
-        return res.status(400).json({
+        return res.status(401).json({
           ok: false,
           message: `Incorrect email - ${body.email}`
         });
       }
 
-      if ( !bcrypt.compareSync(body.password, user.password) ) {
-        return res.status(400).json({
+      if (!bcrypt.compareSync(body.password, user.password)) {
+        return res.status(401).json({
           ok: false,
           message: `Incorrect password - ${body.password}`
         });
       }
 
       // create token
+      const token = jwt.sign({ user }, process.env.JWT_KEY, {
+        expiresIn: "4h"
+      });
+
+      user.password = ':-)';    // hided response password
 
       res.status(200).json({
         ok: true,
         message: "User found",
-        user: user,
+        user,
+        token,
         id: user._id
       });
     })
@@ -40,7 +47,6 @@ router.post("/", (req, res, next) => {
         error: err
       });
     });
-
 });
 
 module.exports = router;
