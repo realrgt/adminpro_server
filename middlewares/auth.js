05f7cf7e3a5c9a337e65fe78ecb2cc737ac70c43
realrgt <ergitorubildo@gmail.com>
2020-03-08 10:46:55 +0200

    check auth

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+// ================================
+// Verify Token
+// ================================
+exports.verifyToken = (req, res, next) => {
+
+    const token = req.query.token;
+
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+  
+      if (err) {
+          return res.status(401).json({
+              ok: false,
+              message: 'Invalid token',
+              error: err
+          });
+      }
+
+      req.userToken = decoded.user; // assigning decoded user to the request
+  
+      next();
+  
+    });
+
+}
