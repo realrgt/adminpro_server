265e0ef2e51fb8c8db9fda6325e3a4b82c6419b3
realrgt <ergitorubildo@gmail.com>
2020-03-25 13:15:18 +0200

    search feature added

--- routes/search.js
+++ routes/search.js
@@ -6,9 +6,47 @@ const Doctor = require("../models/doctor");
 const User = require("../models/user");
 
 // ==========================
-// Collection search 
+// Collection search
 // ==========================
-// router.get('/collection/:table/:keyword')
+router.get("/collection/:table/:keyword", (req, res, next) => {
+  var table = req.params.table;
+  var keyword = req.params.keyword;
+  var regex = new RegExp(keyword, "i");
+
+  let promise;
+
+  switch (table) {
+    case "users":
+      promise = findUsers(regex);
+      break;
+
+    case "hospitals":
+      promise = findHospitals(regex);
+      break;
+
+    case "doctors":
+      promise = findDoctors(regex);
+      break;
+
+    default:
+      return res.status(400).json({
+        ok: false,
+        message: "The kind of search are: users, hospitals and doctors",
+        error: { message: "Invalid collection provided" }
+      });
+      break;
+  }
+
+  promise.then(docs => {
+
+    res.status(200).json({
+        ok: true,
+        [table]: docs
+    });
+
+  });
+
+});
 
 // ==========================
 // GENERAL SEARCH
